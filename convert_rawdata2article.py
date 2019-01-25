a66773839d596591415220fbcbff0978cfbb3625
Masataka Hisasue <masataka.hisasue@optim.co.jp>
2019-01-25 18:04:41 +0900

    feat: rawdata system

---
+++ convert_rawdata2article.py
@@ -0,0 +1,33 @@
+import glob
+import os
+
+def get_rawdata_data(rawdata):
+    main = rawdata.split('{{main}}:')[1].strip()
+    rawdata = rawdata.split('{{main}}:')[0]
+
+    urlname = rawdata.split('{{urlname}}:')[1].strip()
+    rawdata = rawdata.split('{{urlname}}:')[0]
+
+    title = rawdata.split('{{title}}:')[1].strip()
+
+    return title, urlname, main
+
+if __name__ == '__main__':
+
+    rawdata_list = sorted(glob.glob('rawdata/*'))
+
+    for rawdata_path in rawdata_list:
+        with open(rawdata_path) as rawdata_file:
+            rawdata = rawdata_file.read()
+
+        title, urlname, main = get_rawdata_data(rawdata)
+
+        with open('article_template') as article_template_file:
+            article_template = article_template_file.read()
+
+        article = article_template.replace('{{title}}', title)
+        article = article.replace('{{main}}', main)
+
+        output_path = os.path.join('docs', 'contents', urlname)
+        with open(output_path, 'w') as outputfile:
+            outputfile.write(article)
