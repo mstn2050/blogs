f49ae987eb534bd449fcdfd2764239836912bb12
Masataka Hisasue <masataka.hisasue@optim.co.jp>
2019-01-25 18:57:16 +0900

    feat: auto write index.html

--- convert_rawdata2article.py
+++ convert_rawdata2article.py
@@ -15,6 +15,9 @@ def get_rawdata_data(rawdata):
 if __name__ == '__main__':
 
     rawdata_list = sorted(glob.glob('rawdata/*'))
+    content_list = ''
+
+    content_template = '<h3 id="{{id}}"><a href="https://mstn2050.github.io/blogs/contents/{{urlname}}">{{title}}</a></h3>'
 
     for rawdata_path in rawdata_list:
         with open(rawdata_path) as rawdata_file:
@@ -31,3 +34,17 @@ if __name__ == '__main__':
         output_path = os.path.join('docs', 'contents', urlname)
         with open(output_path, 'w') as outputfile:
             outputfile.write(article)
+
+        content = content_template.replace('{{id}}', title)
+        content = content.replace('{{title}}', title)
+        content = content.replace('{{urlname}}', urlname)
+
+        content_list += content + os.linesep
+
+    with open('index_template') as index_templete_file:
+        index_templete = index_templete_file.read()
+
+    index_html = index_templete.replace('{{content}}', content_list.strip())
+
+    with open('docs/index.html', 'w') as outputfile:
+        outputfile.write(index_html)
